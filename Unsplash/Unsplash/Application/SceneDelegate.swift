2de8a056620ed5fdfd55dbae25be70027f3b6752
nhauet99 <nhauet99@gmail.com>
2020-08-17 13:19:14 +0700

    init project add app icon

---
+++ Unsplash/Unsplash/Application/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Unsplash
+//
+//  Created by Nha Pham on 8/17/20.
+//  Copyright © 2020 Nha Pham. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
